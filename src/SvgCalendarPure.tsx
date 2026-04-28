15ed38ec3a25f63328084201b37eb4fc324695e9
j- <j@skeoh.com>
2026-04-28 15:26:03 +1000

    Update dimensions for landscape and portrait orientations

--- src/SvgCalendarPure.tsx
+++ src/SvgCalendarPure.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import { useId, type FC } from 'react';
 
 export type SvgCalendarPureProps = {
@@ -15,42 +16,51 @@ export const SvgCalendarPure: FC<SvgCalendarPureProps> = ({
 
   const colCount = 14;
   const rowCount = Math.ceil(daysInYear / colCount);
-  const size = 20;
+  const size = 24;
 
   return (
-    <svg width={colCount * size} height={rowCount * size}>
-      <defs>
-        <symbol id={idPast} viewBox="0 0 16 16">
-          <path d="M2 2 14 14M2 14 14 2" />
-        </symbol>
-        <symbol id={idFuture} viewBox="0 0 16 16">
-          <circle cx={8} cy={8} r={2} opacity={0.5} />
-        </symbol>
-      </defs>
+    <Box sx={{
+      '& > svg': {
+        width: '100%',
+        maxWidth: 'calc(100vw - 4rem)',
+        height: '100%',
+        maxHeight: 'calc(100vh - 4rem)',
+      },
+    }}>
+      <svg viewBox={`0 0 ${colCount * size} ${rowCount * size}`}>
+        <defs>
+          <symbol id={idPast} viewBox="0 0 16 16">
+            <path d="M4 4 12 12M4 12 12 4" />
+          </symbol>
+          <symbol id={idFuture} viewBox="0 0 16 16">
+            <circle cx={8} cy={8} r={2} opacity={0.5} />
+          </symbol>
+        </defs>
 
-      {Array.from({ length: rowCount }, (_, row) => (
-        Array.from({ length: colCount }, (__, col) => {
-          const i = row * colCount + col;
+        {Array.from({ length: rowCount }, (_, row) => (
+          Array.from({ length: colCount }, (__, col) => {
+            const i = row * colCount + col;
 
-          if (i >= daysInYear) return null;
+            if (i >= daysInYear) return null;
 
-          const isPast = i < dayOfYear;
+            const isPast = i < dayOfYear;
 
-          return (
-            <use
-              key={`${col},${row}`}
-              href={`#${isPast ? idPast : idFuture}`}
-              x={col * size + 1}
-              y={row * size + 1}
-              width={size - 2}
-              height={size - 2}
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={1}
-            />
-          );
-        })
-      ))}
-    </svg>
+            return (
+              <use
+                key={`${col},${row}`}
+                href={`#${isPast ? idPast : idFuture}`}
+                x={col * size + 1}
+                y={row * size + 1}
+                width={size - 2}
+                height={size - 2}
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={0.5}
+              />
+            );
+          })
+        ))}
+      </svg>
+    </Box>
   );
 };
